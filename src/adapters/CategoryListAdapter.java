68c24c58042d51cd294c0507fef43dcb304175e7
Samer Shatta <samer.shatta@gmail.com>
2015-11-19 20:37:32 +0200

    link categories Grid

---
+++ src/adapters/CategoryListAdapter.java
@@ -0,0 +1,54 @@
+package adapters;
+
+import java.util.List;
+
+import com.brainSocket.aswaq.R;
+import com.brainSocket.models.CategoryModel;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+
+public class CategoryListAdapter extends BaseAdapter{
+	private Context context;
+	private List<CategoryModel> categories;
+	
+	public CategoryListAdapter(Context context,List<CategoryModel> categories) {
+		// TODO Auto-generated constructor stub
+		this.context=context;
+		this.categories=categories;
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return categories.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		LayoutInflater inflater=(LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		if(convertView==null)
+			convertView=inflater.inflate(R.layout.item_main_grid_category, parent, false);
+		EditText lblCategoryName=(EditText) convertView.findViewById(R.id.lblCategoryName);
+		lblCategoryName.setText(categories.get(position).getName());
+		return convertView;
+	}
+
+}
--- src/com/brainSocket/fragments/FragMain.java
+++ src/com/brainSocket/fragments/FragMain.java
@@ -1,6 +1,7 @@
 package com.brainSocket.fragments;
 
 import java.util.List;
+
 import com.brainSocket.aswaq.AswaqApp;
 import com.brainSocket.aswaq.HomeCallbacks;
 import com.brainSocket.aswaq.R;
@@ -10,101 +11,105 @@ import com.brainSocket.data.ServerAccess;
 import com.brainSocket.data.ServerResult;
 import com.brainSocket.models.CategoryModel;
 import com.brainSocket.models.SlideModel;
+
 import enums.FragmentType;
+import adapters.CategoryListAdapter;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.GridView;
 
-public class FragMain extends Fragment implements OnClickListener{
+public class FragMain extends Fragment implements OnClickListener {
 	private HomeCallbacks homeCallbacks;
 	private Button btnAddAdvertise;
-	
+	private GridView gridViewCategories;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		return inflater.inflate(R.layout.frag_main, container, false);
 	}
-	
+
 	@Override
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onViewCreated(view, savedInstanceState);
 		init();
 	}
-	
-	private void init()
-	{
-		homeCallbacks=(HomeCallbacks)getActivity();
-		DataStore.getInstance().attemptGetPageComponents(ServerAccess.MAIN_CATEGORY_ID, getPageComponentsCallback);
-		btnAddAdvertise=(Button)getActivity().findViewById(R.id.btnAddAdvertise);
+
+	private void init() {
+		homeCallbacks = (HomeCallbacks) getActivity();
+		gridViewCategories=(GridView)getActivity().findViewById(R.id.gridViewCategories);
+		DataStore.getInstance().attemptGetPageComponents(
+				ServerAccess.MAIN_CATEGORY_ID, getPageComponentsCallback);
+		btnAddAdvertise = (Button) getActivity().findViewById(
+				R.id.btnAddAdvertise);
 		btnAddAdvertise.setOnClickListener(this);
 	}
-	
-	private void search()
-	{
-		String keyword="computer";
-		if(AswaqApp.isEmptyOrNull(keyword))
-		{
+
+	private void search() {
+		String keyword = "computer";
+		if (AswaqApp.isEmptyOrNull(keyword)) {
 			// set text error on the text box and request for focus
-		}
-		else
-		{
-			DataStore.getInstance().attemptSearchFor(keyword,getSearchResultsCallback);
+		} else {
+			DataStore.getInstance().attemptSearchFor(keyword,
+					getSearchResultsCallback);
 		}
 	}
-	
-	private DataRequestCallback getSearchResultsCallback=new DataRequestCallback() {
-		
+
+	private DataRequestCallback getSearchResultsCallback = new DataRequestCallback() {
+
 		@Override
 		public void onDataReady(ServerResult data, boolean success) {
 			// TODO Auto-generated method stub
-			
+
 		}
 	};
-	
-	private void displaySubCategories(int parentCategoryId)
-	{
+
+	private void displaySubCategories(int parentCategoryId) {
 		homeCallbacks.loadFragment(FragmentType.SubCategories);
 	}
-	
-private DataRequestCallback getPageComponentsCallback=new DataRequestCallback() {
-		
+
+	private DataRequestCallback getPageComponentsCallback = new DataRequestCallback() {
+
 		@Override
 		public void onDataReady(ServerResult data, boolean success) {
 			// TODO Auto-generated method stub
-			if(success)
-			{
-				if(data.getFlag()==ServerAccess.ERROR_CODE_done)
-				{
-					List<CategoryModel> categories=(List<CategoryModel>)data.getValue("categories");
-					homeCallbacks.showToast(Integer.toString(categories.size()));
+			if (success) {
+				if (data.getFlag() == ServerAccess.ERROR_CODE_done) {
+					List<CategoryModel> categories = (List<CategoryModel>) data
+							.getValue("categories");
+					homeCallbacks
+							.showToast(Integer.toString(categories.size()));
+
+					List<SlideModel> slides = (List<SlideModel>) data
+							.getValue("slides");
+					//homeCallbacks.showToast(Integer.toString(slides.size()));
+					CategoryListAdapter categoryListAdapter=new CategoryListAdapter(getActivity(), categories);
+					gridViewCategories.setAdapter(categoryListAdapter);
 					
-					List<SlideModel> slides=(List<SlideModel>)data.getValue("slides");
-					homeCallbacks.showToast(Integer.toString(slides.size()));
-				}
-				else
-				{
+				} else {
 					homeCallbacks.showToast("error in getting categories");
 				}
 			}
 		}
 	};
 
-@Override
-public void onClick(View v) {
-	// TODO Auto-generated method stub
-	int viewId=v.getId();
-	switch(viewId)
-	{
-	case R.id.btnAddAdvertise:
-		homeCallbacks.loadFragment(FragmentType.AddAdvertise);
-		break;
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		int viewId = v.getId();
+		switch (viewId) {
+		case R.id.btnAddAdvertise:
+			homeCallbacks.loadFragment(FragmentType.AddAdvertise);
+			break;
+		}
 	}
-}
 
 }
